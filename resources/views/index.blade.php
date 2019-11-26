564a3b9174904ff6e33852cb327986279c2beab0
Kiki1985 <miroslav.dojchinovic@gmail.com>
2019-11-26 20:40:03 +0100

    expecting finish date formated in javaScript code.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
 @foreach($tasks as $task)
 	<tr>
 		<td><a href="tasks/{{$task->id}}">{{$task->title}}</a></td>
-		<td><span id="span_{{$task->id}}">{{\Carbon\Carbon::parse($task->expected_finish_date)->toFormattedDateString()}}</span></td>
+		<td><span id="span_{{$task->id}}">{{--{{\Carbon\Carbon::parse($task->expected_finish_date)->toFormattedDateString()}}--}}</span></td>
 	</tr>
 @endforeach
 </table>
@@ -44,14 +44,17 @@ function renderHTML(t){
 	xhttp.send();
 	xhttp.onreadystatechange = function(){
 		if(xhttp.readyState === 4){
+			
 			for(var i in t){
+				const months = ["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 				var expected_finish_date = t[i].expected_finish_date;
 				expected_finish_date = new Date(expected_finish_date);
 				var current_date = new Date(xhttp.responseText);
 				var Difference_In_Time = expected_finish_date.getTime() - current_date.getTime();
 				var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+				var formatted_date = months[expected_finish_date.getMonth()] + ", " + expected_finish_date.getDate()+ " " + expected_finish_date.getFullYear();
 				//alert('id ' +t[i].id +' diference in days '+Difference_In_Days);
-
+				document.getElementById('span_'+t[i].id).innerHTML = formatted_date;
 				if(Difference_In_Days <= 10)
 				document.getElementById('span_'+t[i].id).innerHTML = Difference_In_Days+ ' days';
 				if(Difference_In_Days == 1)
