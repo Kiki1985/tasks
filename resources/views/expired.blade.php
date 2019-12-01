a1fea443d56be729049925c6be838a15b2f9c9ce
Kiki1985 <miroslav.dojchinovic@gmail.com>
2019-12-01 10:59:23 +0100

    Expired tasks page

--- resources/views/expired.blade.php
+++ resources/views/expired.blade.php
@@ -1,6 +1,9 @@
 @extends('layout')
 @section('content')
 <h1>Expired tasks</h1>
+@if($expired_tasks->isEmpty())
+<p><i>No expired tasks</i></p>
+@else
 @foreach($expired_tasks ?? '' as $expired_task)
 	<h3>{{$expired_task->title}}</h3>
 	<p>{{$expired_task->description}}</p>
@@ -8,5 +11,6 @@
 	<p>Expired at:{{\Carbon\Carbon::parse($expired_task->expected_finish_date)->toFormattedDateString()}}</p>
 	<hr>
 @endforeach
+@endif
 <a href="/"><button>Tasks</button></a>
 @endsection
