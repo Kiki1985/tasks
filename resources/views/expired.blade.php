bdcc1a2f78c2ffd6d06955ddec9d353bf6cad001
Kiki1985 <miroslav.dojchinovic@gmail.com>
2019-12-01 11:03:10 +0100

    Expired tasks page

--- resources/views/expired.blade.php
+++ resources/views/expired.blade.php
@@ -7,8 +7,8 @@
 @foreach($expired_tasks ?? '' as $expired_task)
 	<h3>{{$expired_task->title}}</h3>
 	<p>{{$expired_task->description}}</p>
-	<p>Created at: {{\Carbon\Carbon::parse($expired_task->created_at)->toFormattedDateString()}}</p>
-	<p>Expired at:{{\Carbon\Carbon::parse($expired_task->expected_finish_date)->toFormattedDateString()}}</p>
+	<p>Created at {{\Carbon\Carbon::parse($expired_task->created_at)->toFormattedDateString()}}</p>
+	<p>Expired {{\Carbon\Carbon::parse($expired_task->expected_finish_date)->toFormattedDateString()}}</p>
 	<hr>
 @endforeach
 @endif
