ed6c23af8b64d2a645cd67dd215bcf15899fc411
Kiki1985 <miroslav.dojchinovic@gmail.com>
2020-01-18 19:23:15 +0100

    current date on two digits month and day

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
-	date = new Date();
-	strDate = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2);
+date = new Date();
+strDate = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2);
 	$.ajaxSetup({
 		headers: {
 			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -42,14 +42,14 @@ $( document ).ready(function() {
 
 	function showTask(value){
 		task = "<div id=task"+value.id+">"+
-					 "<p style='cursor:pointer' id='p"+value.id+"'>"+value.title+"Date to finish: "+value.dateToFinish+"</p>" +
-					 "<div id=div"+value.id+" style='display:none'><hr>" + 
-					 "<p>"+value.description+"</p>" +
-					 "<p>Date to finish: "+value.dateToFinish+"</p>" +
-					 "<p>Created at: "+value.created_at+"</p>" +
-					 "<a href='/tasks/"+value.id+"/edit'><button>Update task</button></a>"+
-					 "<button id='delete"+value.id+"'>Delete task</button><hr>"+
-					 "</div></div>";
+				"<p style='cursor:pointer' id='p"+value.id+"'>"+value.title+"Date to finish: "+value.dateToFinish+"</p>" +
+				"<div id=div"+value.id+" style='display:none'><hr>" + 
+				"<p>"+value.description+"</p>" +
+				"<p>Date to finish: "+value.dateToFinish+"</p>" +
+				"<p>Created at: "+value.created_at+"</p>" +
+				"<a href='/tasks/"+value.id+"/edit'><button>Update task</button></a>"+
+				"<button id='delete"+value.id+"'>Delete task</button><hr>"+
+				"</div></div>";
 		
 		dateToFinish = new Date(value.dateToFinish);
 		dateToFinish = dateToFinish.getFullYear() + "-" + ("0" + (dateToFinish.getMonth() + 1)).slice(-2) + "-" + ("0" + dateToFinish.getDate()).slice(-2);
@@ -62,11 +62,10 @@ $( document ).ready(function() {
 		}
 
 		$('#p'+value.id+'').click(function() {
-		$('#div'+value.id+'').slideToggle("fast");
+			$('#div'+value.id+'').slideToggle("fast");
 		});
 
-		$( document ).ready(function() {
-			$('#delete'+value.id+'').click(function() {
+		$('#delete'+value.id+'').click(function() {
 			$("#task"+value.id+"").remove();
 			if( $('#title').is(':empty') ) {
 	    		$('#msg').html('<i>No tasks yet</i>');
@@ -78,11 +77,10 @@ $( document ).ready(function() {
 	        url: '/delete/'+value.id+'',
 	        type: 'get',
 	       		  });
-			});
 		});
 	}
 		
-		$('#slideToggle').click(function() {
+	$('#slideToggle').click(function() {
 		$('#slideToggleDiv').slideToggle("fast");
 	});
 });
