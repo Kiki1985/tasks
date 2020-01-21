99914a743f97dcd3e122e1e22baf08cebf8ae4ef
Kiki1985 <miroslav.dojchinovic@gmail.com>
2020-01-21 19:13:24 +0100

    task styled

--- public/js/script.js
+++ public/js/script.js
@@ -41,10 +41,10 @@ strDate = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "
 	});
 
 	function showTask(value){
-		task = "<div id=task"+value.id+" style='margin-bottom: 45px;'>"+
+		task = "<div id='task"+value.id+"' style='margin-bottom: 45px;'>"+
 				"<p style='cursor:pointer' id='p"+value.id+"'>"+value.title+"</p>"+
 				"<p> <b><i id='i"+value.id+"'>Date to finish: </i></b>"+value.dateToFinish+"</p>"+
-				"<div id=div"+value.id+" style='display:none'>"+ 
+				"<div id='div"+value.id+"' style='display:none'>"+ 
 				"<p>"+value.description+"</p>"+
 				"<p><b><i>Created at: </i></b>"+value.created_at+"</p>"+
 				"<a href='/tasks/"+value.id+"/edit'><button>Update task</button></a>"+
@@ -73,7 +73,7 @@ strDate = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "
 	    		$('#expMsg').html('<i>No expired tasks</i>');
 			}
 			$.ajax({
-	        url: '/delete/'+value.id+'',
+	        url: '/delete/'+id+'',
 	        type: 'get',
 	       		  });
 		});
