beed17d59dce25b0bc6d3cfde3955a6280df9664
Kiki1985 <miroslav.dojchinovic@gmail.com>
2020-03-11 18:44:04 +0100

    Events called from Task model

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -3,10 +3,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Task;
-use App\Events\TaskCreated;
-use App\Events\TaskDeleted;
-use App\Events\TaskUpdated;
-
 
 class TasksController extends Controller
 {
@@ -22,20 +18,17 @@ class TasksController extends Controller
             return back();
         }
         $task = Task::create($this->validateTask());
-        event(new TaskCreated($task));
         return ($task);
     }
 
     public function destroy(Task $task)
     {
-        event(new TaskDeleted());
         $task->delete();
         return back();
     }
 
     public function update(Task $task)
     {
-        event(new TaskUpdated($task));
         $task->update($this->validateTask());
         return redirect('/tasks');
     }
--- app/Task.php
+++ app/Task.php
@@ -2,10 +2,19 @@
 
 namespace App;
 
+use App\Events\TaskCreated;
+use App\Events\TaskDeleted;
+use App\Events\TaskUpdated;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
     protected $guarded = [];
     protected $dates = ['dateToFinish'];
+    protected $dispatchesEvents = [
+    	'created' =>TaskCreated::class,
+    	'deleted' =>TaskDeleted::class,
+    	'updated' =>TaskUpdated::class
+    ];
 }
